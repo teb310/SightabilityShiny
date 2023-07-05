1218b5e7049ac53aad561aaeb05bb07aa392a6ed
Brush <Tristen.Brush@gov.bc.ca>
2023-07-05 13:25:37 -0700

    Get code working
    Updated to fit Shiny format, relocated code from here to server in app.R, simplified and improved reproducibility

--- Bayesian_condensed.R
+++ Bayesian_condensed.R
@@ -1,81 +1,14 @@
 # Elk Sightability Analysis
+
+runModel <- function(file_path) {
+
 # 1 Load and clean ####
 
-## 1.1 LOAD PACKAGES ####
+## 1.1 LOAD DATA ####
 
-list.of.packages <- c("tidyverse", "lubridate","chron","bcdata", "bcmaps","sf", "rgdal", "readxl", "Cairo", "rjags","coda","OpenStreetMap", "ggmap", "SightabilityModel","truncnorm", "doParallel", "nimble", "xtable", "statip", "R2jags")
-# Check you have them and load them
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-lapply(list.of.packages, require, character.only = TRUE)
+file <- paste0(file_path())
 
-## 1.2 FUNCTIONS ####
-# Name_fixer converts misspelled or abbreviated EPU names to standard names
-name_fixer <- function(x){
-  output <- case_when(
-    grepl("Rainy", x, ignore.case = TRUE) ~ "Rainy-Gray",
-    grepl("Narrow", x, ignore.case = TRUE) ~ "Tzoonie-Narrows",
-    grepl("Desert", x, ignore.case = TRUE) ~ "Deserted-Stakawus",
-    grepl("Cheh", x, ignore.case = TRUE) ~ "Chehalis",
-    grepl("Sech", x, ignore.case = TRUE) ~ "Sechelt Peninsula",
-    grepl("Homa", x, ignore.case = TRUE) ~ "Homathko",
-    grepl("Lois", x, ignore.case = TRUE) ~ "Lois",
-    grepl("Eldr", x, ignore.case = TRUE) ~ "Eldred",
-    grepl("Hasl", x, ignore.case = TRUE) ~ "Haslam",
-    grepl("Dani", x, ignore.case = TRUE) ~ "Powell-Daniels",
-    grepl("Quat", x, ignore.case = TRUE) ~ "Quatam",
-    grepl("Lill", x, ignore.case = TRUE) ~ "Lower Lillooet",
-    grepl("Van", x, ignore.case = TRUE) ~ "Vancouver",
-    grepl("Squam", x, ignore.case = TRUE) ~ "Squamish",
-    grepl("Indian", x, ignore.case = TRUE) ~ "Indian",
-    grepl("Stave", x, ignore.case = TRUE) ~ "Stave",
-    grepl("Theo", x, ignore.case = TRUE) ~ "Theodosia",
-    grepl("Mcnab", x, ignore.case = TRUE) ~ "McNab",
-    grepl("Bear", x, ignore.case = TRUE) ~ "Bear",
-    TRUE ~ x
-  )
-  return(output)
-}
-
-# Standard_survey standardizes survey types
-standard_survey <- function(x){
-  output <- case_when(
-    grepl("incidental", x, ignore.case = TRUE) ~ "Incidental",
-    grepl("telemetry", x, ignore.case = TRUE) ~ "Telemetry",
-    grepl("transect", x, ignore.case = TRUE) ~ "Inventory",
-    grepl("inventory", x, ignore.case = TRUE) ~ "Inventory",
-    grepl("capture", x, ignore.case = TRUE) ~ "Capture",
-    TRUE ~ "Other")
-  return(output)
-}
-
-# compile_sheets binds all sheets in a file (filepath) that follow a certain naming patter (type)
-# E.g. to bind survey data sheets from all years, type should be "Data"
-compile_sheets <- function(filepath,type){
-  sheets <- excel_sheets(filepath)
-  sheets <- subset(sheets, str_detect(sheets, paste0(type)) ==T)
-  output <- data.frame(matrix(ncol = 0, nrow = 0))
-  for(i in 1:length(sheets))
-  {
-    output <- bind_rows(output, read_excel(filepath, sheet = paste0(sheets[i])))
-  }
-  return(output)
-}
-
-## 1.3 LOAD DATA ####
-
-# create input and output folders if you haven't already
-dir.create("C:/Users/TBRUSH/R/SightabilityModels/input")
-dir.create("C:/Users/TBRUSH/R/SightabilityModels/output")
-
-# Set your working directory paths and survey data file path
-
-input <- "C:/Users/TBRUSH/R/SightabilityModels/input"
-output <- "C:/Users/TBRUSH/R/SightabilityModels/output"
-
-file <- "2023_data.xlsx"
-
-setwd(input)
+setwd(input_wd)
 
 # Extract observations from all years
 # If you didn't name your survey data sheets with "Data", replace below
@@ -89,6 +22,8 @@ obs.all$survey.type <- standard_survey(obs.all$survey.type)
 # This acts as a list of all the EPUs that were properly surveyed that year
 eff <- compile_sheets(file, "Summary") %>%
   filter(!is.na(min_count))
+
+eff$EPU <- name_fixer(eff$EPU)
 
 # Save EPU names from reliable source
 EPU.areas <- read_csv("EPU_areas.csv")
@@ -124,45 +59,41 @@ for(i in 1:nrow(sight))
 }
 sight <- bind_rows(sight, sight.dup)
 
-## 1.5 EFFORT ####
-
-setdiff(eff$EPU, EPU.list) # If nothing is returned, names match. Otherwise run name_fixer
-eff$EPU <- name_fixer(eff$EPU)
-
-# Amend EPU.list to only include surveyed EPUs, then assign ID numbers
-EPU.list <- data.frame(EPU = unique(eff$EPU)) %>%
-  mutate(ID = seq(1,length(EPU),1))
-
-eff <- left_join(eff, EPU.list, by="EPU")
-
 ## 1.6 OBSERVATIONAL DATASET ####
 
-# make sure we're only keeping data from EPUs with effort data that year
-obs <- inner_join(obs.all, eff %>% select(year, EPU, ID), by=c("EPU","year")) %>%
+# make sure we're only keeping data from EPUs with summary data that year
+obs <- inner_join(obs.all, eff %>% select(year, EPU), by=c("EPU","year")) %>%
   mutate(
-    stratum = ID,
     subunit = EPU,
-    total = if_else(is.na(total), 0, total),
+    # make sure there are no NAs in observation counts
+    mutate(across(c(cow, calf, spike, bull, UC, total), ~if_else(is.na(.), 0, .))),
     voc = voc,
     .keep="unused") %>%
   # only keep inventory & capture surveys
   filter(survey.type=="Inventory" | survey.type=="Capture")
 
 # make sure all totals = sum of cows, calves, etc
-obs %>%
-  filter(total != (cow+calf+spike+bull+UC)) %>%
-  glimpse()
 
-# If records show up, use code below to add unclassified individuals & re-check
-# obs <- obs %>%
-#   mutate(
-#     unclass = if_else(
-#       total > (cows+calves+spikes+bulls+unclass), total-(cows+calves+spikes+bulls),
-#       unclass),
-#     total = (cows+calves+spikes+bulls+unclass))
-# obs %>%
-#   filter(total != (cows+calves+spikes+bulls+unclass)) %>%
-#   glimpse() # all good now
+obs1 <- obs %>%
+  mutate(UC = if_else(
+      total > (cow+calf+spike+bull+UC), as.numeric(total-(cow+calf+spike+bull)),
+      as.numeric(UC)),
+    total = as.numeric(cow+calf+spike+bull+UC))
+
+## 1.6 EFFORT ####
+
+# Amend EPU.list to only include surveyed EPUs, then assign ID numbers
+EPU.list <- data.frame(EPU = unique(obs$subunit)) %>%
+  mutate(ID = seq(1,length(EPU),1))
+
+eff <- inner_join(eff, EPU.list, by="EPU")
+
+# Add ID to obs
+
+obs <- inner_join(obs, eff %>% select(year, EPU, ID), by=c(c("subunit"="EPU"),"year")) %>%
+  mutate(
+    stratum = ID,
+    .keep="unused")
 
 # 2 Prepare data ####
 
@@ -318,27 +249,19 @@ for (i in 1:nrow(plot.dat)){
 
 ## 2.5 SAVE INPUTS ####
 # JAGS inputs
-save(list = c("sight.dat", "oper.dat", "plot.dat", "scalar.dat", "eff", "scalar.sums"), file = "jags_input.rdata")
+jags_input <- c("sight.dat", "oper.dat", "plot.dat", "scalar.dat", "eff", "scalar.sums")
+save(list = jags_input, file = paste0("jags_input_", format(Sys.time(), "%Y%b%d_%H%M"), ".rdata"))
 # other inputs
-save(list=c("compile_sheets", "name_fixer", "input","output","file","EPU.list","year.ID", "eff"), file="other_inputs.rdata")
+other_inputs <- c("compile_sheets", "name_fixer", "input_wd","output_wd","file","EPU.list","year.ID", "eff")
+save(list= other_inputs, file=paste0("other_inputs_", format(Sys.time(), "%Y%b%d_%H%M"), ".rdata"))
 
-rm(list = ls())
+files_to_keep <- c(jags_input, other_inputs, "other_inputs")
+
+rm(list = setdiff(ls(), files_to_keep))
 
 # 3 Bayesian Analysis ####
 
-## 3.1 LOAD DATA ####
-
-setwd("C:/Users/TBRUSH/R/SightabilityModels/input")
-load("jags_input.rdata")
-load("other_inputs.rdata")
-
-list.of.packages <- c("tidyverse", "lubridate","chron","bcdata", "bcmaps","sf", "rgdal", "readxl", "Cairo", "rjags","coda","OpenStreetMap", "ggmap", "SightabilityModel","truncnorm", "doParallel", "nimble", "xtable", "statip", "R2jags")
-# Check you have them and load them
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-lapply(list.of.packages, require, character.only = TRUE)
-
-## 3.2 SET PARAMETERS ####
+## 3.1 SET PARAMETERS ####
 
 # specify initial values
 inits <-  function() list(bo=runif(1), bvoc=runif(1))
@@ -347,7 +270,7 @@ inits <-  function() list(bo=runif(1), bvoc=runif(1))
 params <- c("bo", "bvoc", "tau.hat")
 
 # MCMC settings
-ni <- 40000 # build to 400009
+ni <- 40000 # build to 40000
 nt <- 2     # 50% thinning rate (discard every 2nd iteration)
 nb <- 20000
 nc <- 3
@@ -359,41 +282,28 @@ bundle.dat <- list(x.tilde=sight.dat$x.tilde, z.tilde=sight.dat$z.tilde, #sight.
                    R=scalar.dat$R, Ngroups=scalar.dat$Ngroups, Nsubunits.yr=scalar.dat$Nsubunits.yr, scalars=scalar.sums, #scalar.dat
                    years=length(unique(plot.dat$yr.plots)), stratums=length(unique(plot.dat$h.plots)))
 
-## 3.3 RUN THE MODEL ####
+## 3.2 RUN THE MODEL ####
 jags_output <- jags(bundle.dat, inits, params, "beta_binom_model_elk2022.txt", nc, ni, nb, nt)
 
-setwd(output)
+setwd(output_wd)
 
 ## 3.4 SAVE OUTPUTS ####
 
-save("jags_output", "scalar.dat", file="jags_output.rdata")
+jags_outputs <- c("jags_output", "scalar.dat")
+save(list = jags_outputs, file=paste0("jags_output_", format(Sys.time(), "%Y%b%d_%H%M"), ".rdata"))
 
-rm(list = ls())
+files_to_keep <- c(jags_outputs, other_inputs)
+rm(list = setdiff(ls(), files_to_keep))
 
 # 4 Plot & Report ####
-## 4.1 LOAD PACKAGES ####
 
-list.of.packages <- c("bayesplot", "tidyverse", "lubridate","chron","bcdata", "bcmaps","sf", "rgdal", "readxl", "Cairo", "rjags","coda","OpenStreetMap", "ggmap", "SightabilityModel","truncnorm", "doParallel", "nimble", "xtable", "statip", "R2jags", "SimplyAgree")
-# Check you have them and load them
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-lapply(list.of.packages, require, character.only = TRUE)
-
-## 4.2 LOAD DATA ####
-# Get inputs
-setwd("C:/Users/TBRUSH/R/SightabilityModels/input")
-load("other_inputs.rdata")
+## 4.1 LOAD DATA ####
+setwd(input_wd)
 
 # Get summary data (i.e. standard estimates) from your excel file
 standard <- compile_sheets(file, "Summary") %>%
   rename(Standard = estimate)
 standard$EPU <- name_fixer(standard$EPU)
-
-# Get model outputs
-setwd(output)
-
-# jags output already includes all years
-load("jags_output.rdata")
 
 ## 4.3 CLEAN OUTPUTS ####
 
@@ -415,7 +325,7 @@ tau.jags[,9] <- round(jags.summary$sd[4:nrow(jags.summary)]/jags.summary$`50%`[4
 
 rm(jags_output)
 
-colnames(tau.jags) <- c("ID", "year.ID", "JAGS","JAGS_lcl_95", "JAGS_ucl_95", "JAGS_lcl_50", "JAGS_ucl_50", "Rhat", "cv") 
+colnames(tau.jags) <- c("ID", "year.ID", "Model","JAGS_lcl_95", "JAGS_ucl_95", "JAGS_lcl_50", "JAGS_ucl_50", "Rhat", "cv") 
 jags_output <- left_join(tau.jags, year.ID, by="year.ID") %>%
   left_join(EPU.list, by="ID") %>%
   select(-year.ID, -ID)
@@ -435,125 +345,57 @@ standard <- standard %>%
 ### 4.3.3 Combine tables ####
 
 # create a dataframe that combines the important elements of all dataframes
-results.all <- left_join(jags_output %>% select(year, EPU, JAGS, JAGS_lcl_95, JAGS_ucl_95, JAGS_lcl_50, JAGS_ucl_50),
+results.all <- left_join(jags_output %>% select(year, EPU, Model, JAGS_lcl_95, JAGS_ucl_95, JAGS_lcl_50, JAGS_ucl_50),
                          standard, 
                          by=c("EPU", "year"))
 
 # uncomment any commented sections below if you're including mHT estimates
 results.long <- pivot_longer(results.all,
-                             c(JAGS,
+                             c(Model,
                                Standard),
-                             names_to = "model",
+                             names_to = "method",
                              values_to = "estimate") %>%
   mutate(lcl_50 =
-           if_else(model == "JAGS", JAGS_lcl_50, as.double(NA))) %>%
+           if_else(method == "Model", JAGS_lcl_50, as.double(NA))) %>%
 
   mutate(ucl_50 =
-           if_else(model == "JAGS", JAGS_ucl_50, as.double(NA))) %>%
+           if_else(method == "Model", JAGS_ucl_50, as.double(NA))) %>%
   mutate(lcl_95 =
-           if_else(model == "JAGS", JAGS_lcl_95, as.double(NA))) %>%
+           if_else(method == "Model", JAGS_lcl_95, as.double(NA))) %>%
   mutate(ucl_95 =
-           if_else(model == "JAGS", JAGS_ucl_95, as.double(NA))) %>%
+           if_else(method == "Model", JAGS_ucl_95, as.double(NA))) %>%
   select(-JAGS_lcl_50,-JAGS_ucl_50, -JAGS_lcl_95,-JAGS_ucl_95)
 
-write.csv(results.all, "Results_wide.csv", row.names = F)
-write.csv(results.long, "Results_long.csv", row.names = F)
+results_filename <- paste0("Results_", format(Sys.time(), "%Y%b%d_%H%M"), ".csv")
 
-## 4.4 FIGURES ####
+setwd(output_wd)
+write.csv(results.long, results_filename, row.names = F)
 
-setwd(output)
+# 
+# ## 4.5 EXTRAS ####
+# 
+# ### 4.5.1 Agreement ####
+# 
+# agree.BS = agree_test(x = results.all$JAGS,
+#                       y = results.all$Standard, 
+#                       delta = 1)
+# print(agree.BS) # 65%
+# agree.BS_plot = plot(agree.BS) +
+#   scale_y_continuous(breaks = c(seq(-300, 400, by = 100)), limits = c(-300, 400))
+# agree.BS_plot
+# 
+# ggsave("Method_agreement.jpeg", width = 10, height = 7, units="in")
+# 
+# # Agreement Table
+# Agreement <- as.data.frame(matrix(NA, 3, 2))
+# 
+# Agreement[1,] <- c("Bayesian vs. Standard", agree.BS$ccc.xy[1])
+# 
+# colnames(Agreement) <- c("Test", "CCC")
+# 
+# write.csv(Agreement,"Agreement.csv", row.names = FALSE)
 
-### 4.4.1 By year ####
-# One figure per year, showing all EPUs
+}
 
-results_by_year = ggplot(results.long, aes(x = EPU, y=estimate, fill=model))+
-  # add in confidence intervals as dotted lines
-  geom_linerange(aes(EPU, ymin = lcl_95, ymax = ucl_95), linetype = 2, position = position_dodge(width = 0.7)) +
-  # uncomment the line below if you want 50% confidence intervals as well
-  geom_linerange(aes(EPU, ymin = lcl_50, ymax = ucl_50), position = position_dodge(width = 0.7) ) +
-  # add in point estimates
-  geom_point(shape=21, size=2.5, position = position_dodge(width = 0.7))+
-  # set theme elements
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-  theme(axis.text.x=element_text(size=10, angle = 90, vjust = .4)) +
-  theme(axis.text.y = element_text(size=11)) +
-  # set y axis title
-  scale_y_continuous("Population Estimate") +  
-  # use greyscale for fill colors
-  scale_fill_grey(start = 0, end = 1) +
-  # create a new graph for each year
-  facet_wrap(vars(year), scales = "free", ncol = 2)
-
-# view
-results_by_year
-# save
-ggsave("Elk_estimates_yearly_all.jpeg", width = 20, height = ceiling(nrow(year.ID)/2)*6, units="in")
-
-
-### 4.4.2 By EPU ####
-# One figure per EPU, showing all years
-
-# filter out EPUs only surveyed once
-results.mult <- results.long %>%
-  group_by(EPU) %>%
-  summarize(nyear = length(unique(year))) %>%
-  right_join(results.long, by="EPU") %>%
-  filter(nyear >1)
-
-results_by_EPU = ggplot(results.mult, aes(x = as.integer(year), y=estimate, fill=model))+
-  # add in confidence intervals as dotted lines
-  geom_linerange(aes(as.integer(year), ymin = lcl_95, ymax = ucl_95), linetype = 2, position = position_dodge(width = 0.3)) +
-  # uncomment the line below if you want 50% confidence intervals as well
-  geom_linerange(aes(as.integer(year), ymin = lcl_50, ymax = ucl_50), position = position_dodge(width = 0.3) ) +
-  # Add a horizontal dotted line at the target population value
-  geom_hline(aes(yintercept=target), linetype = 2, color = 'red', linewidth=0.5) +
-  # Add a trendline for each set of points (colored by method)
-  geom_smooth(aes(color=model), method=lm, se=FALSE, linewidth=0.5, position = position_dodge(width=0.3)) +
-  # Add point estimates
-  geom_point(shape=21, size=2.5, position = position_dodge(width = 0.3))+
-  # Add theme elements
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
-        panel.background = element_blank(), axis.line = element_line(colour = "black")) +
-  theme(axis.text.x=element_text(size=10, angle = 90, vjust = .4)) +
-  theme(axis.text.y = element_text(size=11), ) +
-  theme(legend.key.size = unit(.5, 'in'), ) +
-  # name x and y axes and make sure there's only one label per year on x axis
-  scale_y_continuous("Population Estimate") +
-  scale_x_continuous("Year", breaks=c(unique(results.mult$year))) +
-  # use greyscale for point fill & color
-  scale_fill_grey(start = 0, end = 0.7) +
-  scale_color_grey(start = 0, end = 0.7) +
-  # separate by EPU
-  facet_wrap(vars(EPU), scales="free", ncol=4)
-
-# view (keep in mind scale will be wonky)
-results_by_EPU
-# save (set size big enough to see everything)
-ggsave("Elk_estimates_EPU_allyears.jpeg", width = 20, height = ceiling(nrow(year.ID)/4)*10, units="in")
-
-
-## 4.5 EXTRAS ####
-
-### 4.5.1 Agreement ####
-
-agree.BS = agree_test(x = results.all$JAGS,
-                      y = results.all$Standard, 
-                      delta = 1)
-print(agree.BS) # 65%
-agree.BS_plot = plot(agree.BS) +
-  scale_y_continuous(breaks = c(seq(-300, 400, by = 100)), limits = c(-300, 400))
-agree.BS_plot
-
-ggsave("Method_agreement.jpeg", width = 10, height = 7, units="in")
-
-# Agreement Table
-Agreement <- as.data.frame(matrix(NA, 3, 2))
-
-Agreement[1,] <- c("Bayesian vs. Standard", agree.BS$ccc.xy[1])
-
-colnames(Agreement) <- c("Test", "CCC")
-
-write.csv(Agreement,"Agreement.csv", row.names = FALSE)
-
+output <- runModel(file_path)
 
